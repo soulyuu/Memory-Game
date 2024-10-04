3b3835c6482b27ca71818ad115713c72c49626c8
soulyuu <155056836+soulyuu@users.noreply.github.com>
2024-10-04 14:08:21 +0800

    Add files via upload

--- card.js
+++ card.js
@@ -3,33 +3,28 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let matchedPairs = 0;
+const totalPairs = cards.length / 2;
 
 function flipCard(event) {
   if (lockBoard) return;
-
   const el = event.currentTarget;
-
   if (el === firstCard) return;
 
   el.classList.add('flip');
 
   if (!hasFlippedCard) {
-    // first click
     hasFlippedCard = true;
     firstCard = el;
-
     return;
   }
 
-  // second click
   secondCard = el;
-
   checkForMatch();
 }
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
   isMatch ? disableCards() : unflipCards();
 }
 
@@ -37,18 +32,25 @@ function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
 
-  resetBoard();
+  matchedPairs++;
+
+  
+  if (matchedPairs === totalPairs) {
+    setTimeout(() => {
+      enlargeAllCards();
+    }, 3000); // 延遲一點時間讓最後一張卡片完全翻轉
+  } else {
+    resetBoard();
+  }
 }
 
 function unflipCards() {
   lockBoard = true;
-
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-
     resetBoard();
-  }, 1500);
+  }, 1000);
 }
 
 function resetBoard() {
@@ -56,11 +58,45 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
-(function shuffle() {
+function enlargeAllCards() {
+  cards.forEach(card => {
+    card.style.transition = 'transform 0.5s';
+    card.style.transform = 'scale(1.1)';
+  });
+
+  setTimeout(() => {
+    shrinkAllCards();
+  }, 1000);
+}
+
+function shrinkAllCards() {
+  cards.forEach(card => {
+    card.style.transform = 'scale(1)';
+  });
+
+  setTimeout(() => {
+    flipAllCardsBack(); 
+  }, 2000); 
+}
+
+function flipAllCardsBack() {
+  cards.forEach(card => {
+    card.classList.remove('flip'); 
+    card.addEventListener('click', flipCard); 
+    card.style.transition = '';
+  });
+
+  matchedPairs = 0;
+  shuffle(); 
+  resetBoard();
+}
+
+function shuffle() {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
-})();
+}
 
+shuffle();
 cards.forEach(card => card.addEventListener('click', flipCard));
